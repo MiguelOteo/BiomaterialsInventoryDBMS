dfc622dd1d48e5c01ffd3f6174b113ac285c1643
Miguel Oteo <Miguel Oteo@10.60.88.219>
2019-02-20 16:26:44 +0100

    Text

---
+++ src/UImenu/UIConsole.java
@@ -0,0 +1,33 @@
+package UImenu;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class UIConsole {
+
+	public static void main(String args[]) throws IOException {
+		
+	  try {
+		
+		  BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+		  char option = '0';
+		
+	      while(option != '2') {
+			
+		     option = (char)console.read();
+		     switch(option) {
+
+			     case '1': {
+			    	 break;
+			     }
+			     case '2': {
+			    	 System.out.print("Programe closed");
+			     }
+			 }
+		  }
+	   } catch(Exception e) {
+		   
+	   }
+	}
+}
---
+++ src/db/jdbc/SQLConnect.java
@@ -0,0 +1,16 @@
+package db.jdbc;
+import java.sql.*;
+
+public class SQLConnect {
+
+	public static void main(String args[]) {
+		try {
+			Class.forName("org.sqlite.JDBC");
+			Connection sqlite_connection = DriverManager.getConnection("jdbc:sqlite:./db/company.db"); 
+			sqlite_connection.createStatement().execute("PRAGMA foreign_keys=ON");
+			sqlite_connection.close();
+		} catch(Exception connection_error) {
+			
+		}
+	}
+}
