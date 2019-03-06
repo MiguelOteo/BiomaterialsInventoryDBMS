a905b599193cb5d1b5ec602f6024f8aa92e87b91
Miguel Oteo <Miguel Oteo@10.60.88.130>
2019-03-06 17:21:00 +0100

    Alpha 3.9

--- src/db/model/UtilMethods.java
+++ src/db/model/UtilMethods.java
@@ -11,13 +11,18 @@ public class UtilMethods {
 	// -----> SUM ALL GAINS METHOD <-----
 	
 	// Gets a list calling Search_stored_transactions and sum all the gains from them
-	public float Sum_all_client_gains(SQLManager manager, Client client) {
+	public int Sum_all_client_points(SQLManager manager, Client client) {
 		
-		float transactions_gains = 0;
+		int points_gains = 0;
 		List<Transaction> transactions_list = manager.Search_stored_transactions(client);
-		for(Transaction transaction: transactions_list) {
-		     transactions_gains = transactions_gains + transaction.getGain();	
+		for(Transaction transaction: transactions_list) { 
+		     points_gains = points_gains + points_function(transaction.getGain());	
 		}
-        return transactions_gains;
+        return points_gains;
+	}
+	
+	public int points_function(float gain) {
+		int points = 0;
+		return 0;
 	}
 }
