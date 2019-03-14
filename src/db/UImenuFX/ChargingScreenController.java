5d0ebe2e1e8ecd3661e41838070109b2f0f9b949
Miguel Oteo <Miguel Oteo@DESKTOP-G8BE8QT>
2019-03-14 22:39:11 +0100

    Alpha 4.7 Arreglados algunos errores al introducir usuarios duplicados

--- src/db/UImenuFX/ChargingScreenController.java
+++ src/db/UImenuFX/ChargingScreenController.java
@@ -47,68 +47,74 @@ public class ChargingScreenController implements Initializable {
 		if(tables_exist == false) {
 			manager.getValue().Create_tables();
 		}
-	    
-		// The following if() checks if its a new user or if he is login
-	    if(this.user_type.get() == null) {
 	    	
 	       // List all clients in order to find if he exist to access it	 
 		   List<Client> clients_list = manager.getValue().List_all_clients();
-		   boolean client_exist = false;
+		   Client client_account = null;
 		   for(Client client: clients_list) {
 			   if((client.getName().equals(this.user_name.get())) && (client.getPassword().equals(this.password.get()))) {
-				   client_exist = true;
+				   client_account = client;
 				   break;
 			   }
 		   }
-		   if(client_exist == true) {
-			   // TODO - charge client menu
-			   // TODO - close windows
+		   if(client_account != null) {
+			   if(this.user_type.get() == null) {
+			       // TODO - charge client menu
+			       // TODO - close windows
+			   } else {
+				   // TODO - close charging window
+			   }
 		   } else {
 			   // List all directors in order to find if he exist to access it
 			   List<Director> directors_list = manager.getValue().List_all_directors();
-			   boolean director_exist = false;
+			   Director director_account = null;
 			   for(Director director: directors_list) {
 				   if((director.getDirector_name().equals(this.user_name.get())) && (director.getPassword().equals(this.password.get()))) {
-					   director_exist = true;
+					   director_account = director;
 					   break;
 				   }
 			   }
-			   if(director_exist == true) {
-				   // TODO - charge director menu
-				   // TODO - close windows
+			   if(director_account != null) {
+				   if(this.user_type.get() == null) {
+					      // TODO - charge director menu
+					      // TODO - close windows
+					   } else {
+						   // TODO - close charging window
+					   }
 			   } else {
 				   // List all workers in order to find if he exist to access it
 				   List<Worker> workers_list = manager.getValue().List_all_workers();
-				   boolean worker_exist = false;
+				   Worker worker_account = null;
 				   for(Worker worker: workers_list) {
 					   if((worker.getWorker_name().equals(this.user_name.get())) && (worker.getPassword().equals(this.password.get()))) {
-						   worker_exist = true;
+						   worker_account = worker;
 						   break;
 					   }
 				   }
-				   if(worker_exist == true) {
-					   // TODO - charge director menu
-					   // TODO - close windows
+				   if(worker_account != null) {
+					   if(this.user_type.get() == null) {
+						      // TODO - charge worker menu
+						      // TODO - close windows
+						   } else {
+							   // TODO - close charging window
+						   }
 				   } else {
-					   // TODO - close charging window
+				    	if(this.user_type.get().equals("Client")) {
+				    		boolean insertion_ok = manager.getValue().Insert_new_client_account(this.user_name.get(), this.password.get());
+				    		// TODO - charge client menu
+				    	} else {
+				    		if(this.user_type.get().equals("Director")) {
+				    			boolean insertion_ok = manager.getValue().Insert_new_director(this.user_name.get(), this.password.get());
+				    		    // TODO - charge director menu
+				    		} else {
+				    			if(this.user_type.get().equals("Worker")) {
+				    				boolean insertion_ok = manager.getValue().Insert_new_worker(this.user_name.get(), this.password.get());
+				    				// TODO - charge worker menu
+				    			}
+				    		}
 				   }
 			   }  
 		   }
-	    } else {
-	    	if(this.user_type.get().equals("Client")) {
-	    		boolean insertion_ok = manager.getValue().Insert_new_client_account(this.user_name.get(), this.password.get());
-	    		// TODO - charge client menu
-	    	} else {
-	    		if(this.user_type.get().equals("Director")) {
-	    			boolean insertion_ok = manager.getValue().Insert_new_director(this.user_name.get(), this.password.get());
-	    		    // TODO - charge director menu
-	    		} else {
-	    			if(this.user_type.get().equals("Worker")) {
-	    				boolean insertion_ok = manager.getValue().Insert_new_worker(this.user_name.get(), this.password.get());
-	    				// TODO - charge worker menu
-	    			}
-	    		}
-	    	}
 	    }
 	}
 	
---
+++ src/db/UImenuFX/ClientMenuController.java
@@ -0,0 +1,29 @@
+package db.UImenuFX;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+public class ClientMenuController implements Initializable{
+
+	// -----> FXML ATRIBUTES <-----
+	
+	@FXML
+	private AnchorPane menu_window;
+	
+	// -----> ESSENTIAL METHODS <-----
+	
+	public ClientMenuController() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
