159b623a70ad82e39a5baa47d4bcbad544d4ad04
Miguel <Miguel@Miguel>
2019-03-16 23:45:15 +0100

    Alpha 4.9 Avances en el menu director

--- src/db/UImenuFX/DirectorMenuController.java
+++ src/db/UImenuFX/DirectorMenuController.java
@@ -11,6 +11,8 @@ import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
@@ -31,7 +33,28 @@ public class DirectorMenuController implements Initializable{
 		private Pane menu_main_pane;
 		@FXML
 		private JFXButton logOut_buttom;
-
+		@FXML
+		private JFXButton myAccount_buttom;
+		@FXML
+		private JFXButton listAllClients_buttom;
+		@FXML
+		private JFXButton removeClient_buttom;
+		@FXML
+		private JFXButton listAllWorkers_buttom;
+		@FXML
+		private JFXButton addWorker_buttom;
+		@FXML
+		private JFXButton removeWorker_buttom;
+		@FXML
+		private JFXButton listAllTransactions_button;
+		@FXML
+		private JFXButton finantialStatus_buttom;
+		@FXML
+		private ImageView minButtom;
+		@FXML
+		private ImageView exitButtom;
+		@FXML
+		private Label current_pane_option_label;
 		// -----> ESSENTIAL METHODS <-----
 
 		public DirectorMenuController() {
@@ -60,5 +83,11 @@ public class DirectorMenuController implements Initializable{
 				LaunchApplication.stage.show();
 				Stage stage = (Stage) logOut_buttom.getScene().getWindow();
 				stage.close();
+		}
+		
+		@FXML
+		private void min_window(MouseEvent event) {
+			Stage stage = (Stage) menu_main_pane.getScene().getWindow();
+			stage.setIconified(true);
 		}
 }
