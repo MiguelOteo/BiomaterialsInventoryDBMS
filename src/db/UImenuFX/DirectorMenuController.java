a371640afc594d1e1e5e8c89f8f21b5bf394d9c4
Miguel <Miguel@Miguel>
2019-05-08 21:26:47 +0200

    Alpha 8.4

--- src/db/UImenuFX/DirectorMenuController.java
+++ src/db/UImenuFX/DirectorMenuController.java
@@ -71,7 +71,9 @@ public class DirectorMenuController implements Initializable {
 	@FXML
 	private NumberAxis amount_purchased;
 	@FXML
-	private LineChart<?, ?> line_chart;
+	private LineChart<String, Number> line_chart;
+	@FXML
+	private Integer client_id_backup = 0; 
 	@FXML
 	private JFXButton logOut_button;
 	@FXML
@@ -292,6 +294,9 @@ public class DirectorMenuController implements Initializable {
 		transactions_tree_view.getColumns().setAll(client_name, amount, units, transaction_date);
 		transactions_tree_view.setRoot(root);
 		transactions_tree_view.setShowRoot(false);
+		
+		// Since there is a bug with the line chart, the auto size of x axis does not work correctly so i add it manually
+		number.setCategories(FXCollections.observableArrayList("1", "2", "3", "4", "5", "6", "7", "8")); 
 	}
 	
 	// -----> REFRESH TRANSACTION LIST VIEW <-----
@@ -315,19 +320,22 @@ public class DirectorMenuController implements Initializable {
     	if(transaction_object != null) {
     		Integer transaction_id = Integer.parseInt(transaction_object.getValue().transaction_id.getValue());
     		Client client = manager_object.Search_transaction_by_id(transaction_id).getClient();
-            List<Transaction> transactions_list = manager_object.Search_stored_transactions(client);
-            series = new XYChart.Series();
-            series.getData().clear();
-            series.getData().add(new XYChart.Data("1", transactions_list.get(transactions_list.size() - 5).getGain()));
-           	series.getData().add(new XYChart.Data("2", transactions_list.get(transactions_list.size() - 4).getGain()));
-           	series.getData().add(new XYChart.Data("3", transactions_list.get(transactions_list.size() - 3).getGain()));
-           	series.getData().add(new XYChart.Data("4", transactions_list.get(transactions_list.size() - 2).getGain()));
-           	series.getData().add(new XYChart.Data("5", transactions_list.get(transactions_list.size() - 1).getGain()));  
-           	series.setName("Payment $");
-           	line_chart.getData().clear();
-            line_chart.getData().add(series);
+    		if(client.getClient_id() != client_id_backup) {
+    			List<Transaction> transactions_list = manager_object.Search_stored_transactions(client);
+    			series = new XYChart.Series();
+            	series.getData().clear();
+            	for(int count = 1; count <= 8 && count < transactions_list.size(); count++) {
+           			series.getData().add(new XYChart.Data(String.valueOf(transactions_list.size() - count) , transactions_list.get(transactions_list.size() - count).getGain()));
+           		}
+           		series.setName("Payment $");
+           		line_chart.setTitle(client.getName() + "'s transactions record");
+           		line_chart.getData().clear();
+           		line_chart.getData().add(series);
+           		series.getNode().setStyle("-fx-stroke: #4f90a5;"); 
+           		client_id_backup = client.getClient_id();
+    		}
     	}
-    }
+	}
 	
 	@FXML
 	private void list_all_clients_button(MouseEvent event) throws IOException {
