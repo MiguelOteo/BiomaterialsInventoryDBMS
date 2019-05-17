ff291d2080defe234048537fc9dd985d729d7610
Miguel Oteo <Miguel Oteo@DESKTOP-G8BE8QT>
2019-05-17 19:36:32 +0200

    Alpha 9.8.1

--- src/db/UImenuFX/ClientMainMenuController.java
+++ src/db/UImenuFX/ClientMainMenuController.java
@@ -3,7 +3,9 @@ package db.UImenuFX;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import javafx.animation.PauseTransition;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.image.Image;
@@ -12,10 +14,15 @@ import javafx.util.Duration;
 
 public class ClientMainMenuController implements Initializable{
 	
+	 final Image image_1 = new Image("file:client-categories.png"); 
+	 final Image image_0 = new Image("file:client-main.png"); 
+	
 	// -----> FXML ATRIBUTES <-----
 	
 	@FXML
 	private ImageView image_view;
+	@FXML
+	private ImageView image_view_hover;
 	
 	// -----> ESSENTIAL METHODS <-----
 	
@@ -25,9 +32,12 @@ public class ClientMainMenuController implements Initializable{
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		PauseTransition wait = new PauseTransition(Duration.seconds(4));
-		Image image = new Image("file:src.IconPictures/client_categories.png");
-		wait.setOnFinished((event_handler) -> image_view.setImage(image));
-		wait.play();
-	}
+		
+		KeyFrame kf1 = new KeyFrame(Duration.seconds(5), new KeyValue(image_view_hover.opacityProperty(), 0));
+        KeyFrame kf2 = new KeyFrame(Duration.seconds(0.5), new KeyValue(image_view_hover.opacityProperty(), 0.2));
+        KeyFrame kf3 = new KeyFrame(Duration.seconds(0.5), new KeyValue(image_view_hover.opacityProperty(), 0.4));
+        Timeline timelineOn = new Timeline(kf1, kf2, kf3);
+        timelineOn.setCycleCount(Timeline.INDEFINITE);
+        timelineOn.play();
+	}	
 }
