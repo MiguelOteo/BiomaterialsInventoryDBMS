343bb3d004f646159789534ffd17b10db945e40b
Miguel <Miguel@Miguel>
2019-05-28 13:35:12 +0200

    Alpha 10.15

--- src/db/ConsoleMains/AddTransaction.java
+++ src/db/ConsoleMains/AddTransaction.java
@@ -18,7 +18,7 @@ public class AddTransaction {
 		SQLManager manager = new SQLManager();
 		boolean everything_ok = manager.Stablish_connection();
 		
-		/*Utility utility = new Utility("cold", "yes", "no", (float)34.7, (float)65.85);
+		Utility utility = new Utility("cold", "yes", "no", (float)34.7, (float)65.85);
 		Integer utility_id = manager.Insert_new_utility(utility);
 		utility = manager.Search_utility_by_id(utility_id);
 		
@@ -27,19 +27,21 @@ public class AddTransaction {
 		maintenance = manager.Search_maintenance_by_id(maintenance_id);
 		
 		List<Biomaterial> biomaterial_list = new LinkedList<Biomaterial>();
-		Biomaterial biomaterial = new Biomaterial(utility, maintenance, "Plastic", (float)34, 30, Date.valueOf("3424-06-03"));
+		Biomaterial biomaterial = new Biomaterial(utility, maintenance, "Plastic", (float)34, 35, Date.valueOf("3424-06-03"));
+		Biomaterial biomaterial2 = new Biomaterial(utility, maintenance, "Metal", (float)4, 10, Date.valueOf("3424-06-03"));
+		Biomaterial biomaterial3 = new Biomaterial(utility, maintenance, "KK", (float)344, 30, Date.valueOf("3424-06-03"));
 		Integer biomaterial_id = manager.Insert_new_biomaterial(biomaterial);
 		biomaterial = manager.Search_biomaterial_by_id(biomaterial_id);
 		biomaterial_list.add(biomaterial);
 		
-		// Transaction(Float gain, Integer client_id, Integer units, Integer product_id)
+		//Transaction(Float gain, Integer client_id, Integer units, Integer product_id);
 		Client client = manager.Search_client_by_id(2);
 		Transaction transaction = new Transaction((float)10, 34243, biomaterial_list, client);
-		manager.Insert_new_transaction(transaction);*/
+		manager.Insert_new_transaction(transaction);
 		
 		List<Transaction> transaction_list = manager.List_all_transactions();
-		for(Transaction transaction: transaction_list) {
-			System.out.println(transaction);
+		for(Transaction transaction2: transaction_list) {
+			System.out.println(transaction2);
 		}
 	}
 } 
